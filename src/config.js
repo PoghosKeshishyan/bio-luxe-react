31e0f3835bafe0d3bafb8310cdeb81891ab26a54
PoghosKeshishyan <poghos877@gmail.com>
2025-11-30 23:26:39 +0400

    first commit

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // const BACKEND_API_URL = 'http://localhost:8000/';
 // const BACKEND_API_URL = 'https://bioluxe-express-api.vercel.app/';
 // const BACKEND_API_URL = "http://46.182.172.161:7075/";
-const BACKEND_API_URL = "http://vpsusr11.dotigo.am";
+const BACKEND_API_URL = "https://vpsusr11.dotigo.am";
 
 export {
     BACKEND_API_URL,
